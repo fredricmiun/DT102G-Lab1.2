a4964320b7c15401e750dee4abcbbd7daa30ccde
iamfredric <fredricfarholt@live.se>
2020-01-27 15:15:15 +0100

    Testar git

--- Lab1/wwwroot/js/JavaScript.js
+++ Lab1/wwwroot/js/JavaScript.js
@@ -1,1 +1,14 @@
-﻿console.log("Hello");
+﻿"use strict";
+const uri = window.location.pathname.split("/");
+function active_menu() {
+    var aElement = document.getElementsByClassName("nav-item");
+    aElement[0].classList.add("active");
+    var i = 0;
+    for (i; i < aElement.length; i++) {
+        if (aElement[i].getAttribute("href") == uri[1]) {
+            aElement[0].classList.remove("active");
+            aElement[i].classList.add("active");
+        }
+    }
+}
+active_menu();
